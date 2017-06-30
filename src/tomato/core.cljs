b0ce676f1cf0af664d50d1986713aef32f894713
Valentin Golev <me@va1en0k.net>
2017-06-30 20:04:55 +0300

    show errors

--- src/tomato/core.cljs
+++ src/tomato/core.cljs
@@ -2,6 +2,7 @@
   (:require [rum.core :as rum]
             [cljs.js]
             [promesa.core :as p :include-macros true]
+            [replumb.common]
 
             [tomato.eval :as ev]
             [linked.core :as linked]))
@@ -16,17 +17,17 @@
                                        [:circle {:r  r
                                                  :cx cx
                                                  :cy cy}])}
-                     (gensym) {:form '(circle 10 [10 20])}
-                     (gensym) {:form '(circle 10 [16 25])})}))
+                     (gensym) {:form '(circle 10 [10 20] 1)}
+                     (gensym) {:form '(ciarcle 10 [16 25])})}))
 
-(defonce element-values
-         (ev/async-map-atom ::element-values
-                            (fn [state]
-                              (p/map
-                                #(into (linked.core/map)
-                                       (map vector (keys (:forms state)) %))
-                                (ev/eval-forms (map (comp :form second) (:forms state)))))
-                            app-state))
+(def element-values
+  (ev/async-map-atom ::element-values
+                     (fn [state]
+                       (p/map
+                         #(into (linked.core/map)
+                                (map vector (keys (:forms state)) %))
+                         (ev/eval-forms (map (comp :form second) (:forms state)))))
+                     app-state))
 
 (defn maybe-read [str]
   (try
@@ -52,25 +53,56 @@
        (catch js/Error e
          nil)))])
 
+(defn show-success [v]
+  [:pre {:style {:background-color "#b0f0a6"
+                 :max-height       "5em"}}
+   (:value v)])
+
+(defn show-fail [v]
+  [:pre {:style {:background-color "#fe7c66"
+                 :max-height       "5em"}}
+   (println v)
+   (replumb.common/extract-message true (:error v))])
+
+(defn show-result [v]
+  (cond
+    (nil? v) nil
+    (:success? v) (show-success v)
+    :default (show-fail v)))
+
 (rum/defc form-editor < rum/reactive {:key-fn #(identity %)}
-  [key form-atom]
-  (let [change-code (fn [v]
-                      (swap! form-atom assoc :form v))]
-    [:textarea
-     {:style     {:width "100%" :height "100%"}
-      :on-change #(change-code (.. % -target -value))
-      :value     (str (:form (rum/react form-atom)))}]))
+  [key form-atom value-atom]
+  (let [change-code! (fn [v]
+                       (swap! form-atom assoc :form v))
+        form (rum/react form-atom)
+        result (rum/react value-atom)
+        error? (not (or (nil? result) (:success? result)))]
+    [:div {:style {:margin-bottom "10px"}}
+     [:textarea
+      {:style     {:width "100%" :height "100%" :display "block" :box-sizing "border-box"}
+       :on-change #(change-code! (.. % -target -value))
+       :value     (str (:form form))}]
+     [:div
+      (if (:warning result)
+        [:pre
+         {:style {:background-color "#f4ff65" :padding "2px"}}
+         (:warning result)]
+        nil)
+      (show-result result)]]))
 
 (rum/defc code-editor < rum/reactive []
   [:div
    (for [form (:forms (rum/react app-state))]
-     (form-editor (first form) (rum/cursor-in app-state [:forms (first form)])))])
+     (form-editor
+       (first form)
+       (rum/cursor-in app-state [:forms (first form)])
+       (rum/cursor-in element-values [(first form)])))])
 
 
 (rum/defc app-area []
   [:div {:style {:display "flex" :height "300px"}}
    [:div {:style {:flex "0 0 65%"}} (drawing-area)]
-   [:div {:style {:flex "1"}} (code-editor)]])
+   [:div {:style {:flex "1 1 35%" :width "200px"}} (code-editor)]])
 
 
 (rum/mount
